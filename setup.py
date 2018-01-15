8c857321fb453a24e44b91972f63c8a975478f42
Kantouzin <kantouzin0113@gmail.com>
2018-01-15 10:13:47 +0900

    Update "setup.py"

--- setup.py
+++ setup.py
@@ -1,25 +1,32 @@
-# -*- coding: utf-8 -*-
-
-# Learn more: https://github.com/kennethreitz/setup.py
+# coding: utf-8
 
 from setuptools import setup, find_packages
 
 
+NAME = "sci_comp"
+VERSION = "0.1.0"
+DESCRIPTION = "Sample package for Python-Guide.org"
+AUTHOR = "Kantouzin"
+AUTHOR_EMAIL = "kantouzin0113@gmail.com"
+URL = "https://github.com/Kantouzin/sci_comp"
+
+REQUIRES = ["numpy"]
+
 with open('README.rst') as f:
-    readme = f.read()
+    README = f.read()
 
 with open('LICENSE') as f:
-    license = f.read()
+    LICENSE = f.read()
 
 setup(
-    name='sample',
-    version='0.1.0',
-    description='Sample package for Python-Guide.org',
-    long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
-    license=license,
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=README,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    install_requires=REQUIRES,
+    url=URL,
+    license=LICENSE,
     packages=find_packages(exclude=('tests', 'docs'))
 )
-
